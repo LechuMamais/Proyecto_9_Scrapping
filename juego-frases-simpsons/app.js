61df47efcd8a72f8b5f53b8be253dd02ac0a903c
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-03-01 16:46:11 +0100

    Added gids to game over, css refactored

--- juego-frases-simpsons/app.js
+++ juego-frases-simpsons/app.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 let frases;
 let puntaje = 0;
+let vidas = 3;
 let fraseMostrada;
 
 // Petición a nuestra API!
@@ -39,7 +40,7 @@ function crearHome() {
     borrarGameContainer(); // Si estabamos jugando, primero borrar todo lo pertinente al juego.
     const appDiv = document.getElementById("app");
     appDiv.innerHTML = `
-        <div id="home-container">
+        <div id="home-container" class="box-shadow flex">
             <h1>¡Juego de Frases de Los Simpsons!</h1>
             <button class="start-button button" onclick="comenzarJuego('principales')">Comienza el juego -- easy</button>
             <button class="start-button button" onclick="comenzarJuego('todos')">Para mas placer --- hard</button>
@@ -57,6 +58,7 @@ function crearGameContainer() {
     const appDiv = document.getElementById("app");
     const gameContainerDiv = document.createElement("div");
     gameContainerDiv.id = "game-container";
+    gameContainerDiv.className = "box-shadow flex";
     appDiv.appendChild(gameContainerDiv);
     mostrarBackToHome();
 }
@@ -81,10 +83,32 @@ function mostrarFrase(frase) {
     const fraseAMostrar = quitarAcentos(frase.frase); // le quitamos los acentos, porque la tipografía no los admite.
     const gameContainer = document.querySelector("#game-container");
     gameContainer.innerHTML += `
-        <div id="frase-container">
+        <div id="frase-container" class="flex">
             <p>"${fraseAMostrar}"</p>
         </div>
     `;
+
+    // vamos a hacer un poquito de responsive acá, si la frase supera cierto largo, que se modifique su font-size.
+    const fraseDiv = document.querySelector("#frase-container>p");
+    if (fraseAMostrar.length > 200) {
+        fraseDiv.style.fontSize = "17px";
+    }else if (fraseAMostrar.length > 150) {
+        fraseDiv.style.fontSize = "18px";
+    }else if (fraseAMostrar.length > 100) {
+        fraseDiv.style.fontSize = "20px";
+    }else if (fraseAMostrar.length > 80) {
+        fraseDiv.style.fontSize = "23px";
+    }else if (fraseAMostrar.length > 60) {
+        fraseDiv.style.fontSize = "25px";
+    }else if (fraseAMostrar.length > 50) {
+        fraseDiv.style.fontSize = "29px";
+    }else if (fraseAMostrar.length > 40) {
+        fraseDiv.style.fontSize = "33px";
+    }else if (fraseAMostrar.length > 30) {
+        fraseDiv.style.fontSize = "35px";
+    }else if (fraseAMostrar.length <=30){
+        fraseDiv.style.fontSize = "39px"
+    }
 }
 
 function mostrarBotones(modo) {
@@ -94,7 +118,7 @@ function mostrarBotones(modo) {
     botonesDiv.innerHTML = `
         
         <h3 id="quien-lo-dijo">Quien lo dijo... </h3>
-        <div id="buttons-container">
+        <div id="buttons-container" class="flex">
             ${frases.map((objeto) => `
                 <button class="select-frase-button button" onclick="manejarSeleccionFrase('${objeto.nombre}', '${modo}')">${objeto.nombre}</button>
             `).join('')}
@@ -115,7 +139,10 @@ function mostrarBackToHome() {
 function mostrarPuntaje() {
     const gameContainer = document.querySelector("#game-container");
     const puntajeDiv = document.createElement("div");
-    puntajeDiv.innerHTML = `<h3>Puntos: ${puntaje}</h3>`;
+    puntajeDiv.innerHTML = `
+        <h3>Puntos: ${puntaje}</h3>
+        <h3>Vidas: ${vidas}</h3>
+    `;
 
     // Eliminar cualquier puntaje anterior del DOM antes de mostrar el nuevo puntaje
     const puntajeAnterior = gameContainer.querySelector("#puntaje");
@@ -126,15 +153,35 @@ function mostrarPuntaje() {
     puntajeDiv.id = "puntaje";
     gameContainer.appendChild(puntajeDiv);
 }
+function gameOver() {
+    // En el gameContainer aparecerá aleatoreamente uno de los gif!
+    const gifIndex = Math.floor(Math.random() *gifsUrl.length);
+    const gameContainer = document.querySelector("#game-container");
+    gameContainer.innerHTML = `
+        <div id="game-over-container">
+            <h3 id="game-over-text">Game Over</h3>
+            <img src="${gifsUrl[gifIndex]}" id="game-over-gif">
+            <div id="buttons-container">
+                <button class="game-over-button button" onclick="crearHome()">Volver</button>
+            </div>
+        </div>
+    `;
+}
 
 function manejarSeleccionFrase(nombre, modo) {
     // Verificar si el nombre del botón coincide con el nombre de la frase mostrada
     if (nombre === fraseMostrada.nombre) {
         puntaje++;
-        mostrarPuntaje();
+        mostrarPuntaje();   // actualiza los puntos que se muestran
+    } else {
+        vidas--;
     }
-    // Pasar a la siguiente frase
-    gameSecuence(modo)
+    if (vidas == -1) {
+        gameOver()  // Si no quedan vidas, se termina el juego
+    } else {
+        // Si todavía tiene vidas, pasar a la siguiente frase (sigue el juego)
+        gameSecuence(modo)
+    }
 }
 
 function quitarAcentos(cadena) {
@@ -162,7 +209,25 @@ function quitarAcentos(cadena) {
     });
 }
 
+const gifsUrl = [
+    "2112.gif",
+    "6392ecddeec3e.gif",
+    "giphy-9.gif",
+    "giphy.webp",
+    "homerbush.gif",
+    "Homero-Los-Simpson.gif",
+    "main-qimg-620ce4751cb12a65372013e1134b4a71.webp",
+    "ralph-wave-1573678867.gif",
+    "ralph-wiggum-smiles-rolls-down-hill.gif",
+    "simpson-1536171233.gif",
+    "simpson-1553589356.gif",
+    "tumblr_8a76648ecc2a2d056802be287db51e27_f37a7f64_400.gif",
+    "tumblr_m7oueziDsw1rwl09fo1_500.gif",
+    "tumblr_m8jikzBXTP1rvn6njo1_500.gif"
+]
+
 function comenzarJuego(modo) {
+    vidas = 3;
     borrarHome()
     crearGameContainer()
     gameSecuence(modo);
