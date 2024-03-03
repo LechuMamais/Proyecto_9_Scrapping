1d9505df19d28b46e018219e09ca46ec7ff49e3f
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-03-03 21:53:28 +0100

    PORT modified: backend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const { connectDB } = require("./src/config/db");
 const FrasesPrincipalesRoutes = require("./src/api/routes/frasesPrincipales.routes");
 const FrasesSecundariosRoutes = require("./src/api/routes/frasesSecundarios.routes");
 const FrasesAleatoriasRoutes = require("./src/api/routes/frasesAleatorias.routes");
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors({
     origin: "https://proyecto-9-scrapping.vercel.app"
@@ -18,4 +18,4 @@ app.use("/api/v1/secundarios", FrasesSecundariosRoutes);
 app.use("/api/v1/game/", FrasesAleatoriasRoutes)
 
 app.use("*", (req,res,next) => {return res.status(404).json("Route Not Found")});
-app.listen(port, ()=>{console.log("Servidor iniciado en http://localhost:" + port)});
+app.listen(PORT, ()=>{console.log("Servidor iniciado en http://localhost:" + PORT)});
